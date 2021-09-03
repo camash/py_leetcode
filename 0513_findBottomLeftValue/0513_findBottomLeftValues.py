9eecd53d9c277bc4535e58684c593200352fa408
shenfeng <shenfeng@MBP>
2021-09-04 00:24:16 +0800

    solve the bug-only for only left leaves of 513.

--- 0513_findBottomLeftValue/0513_findBottomLeftValues.py
+++ 0513_findBottomLeftValue/0513_findBottomLeftValues.py
@@ -13,23 +13,23 @@ def findBottomLeftValue(root: TreeNode):
     def dfs(node: TreeNode, level):
         if node is None:
             return
-        # 如果是左子节点则将层级和节点值放入dict中，其中level需要在当前层级的基础上加1
-        if node.left is not None and node.left.left is None and node.left.right is None:
-            if level+1 not in left_dict.keys():
-                left_dict[level+1] = node.left.val
-        # 然后下进一层，继续看是否有符合的左节点，同时增加层级
+        # 如果是叶子节点则将层级和节点值放入dict中，只有更大的level时才插入
+        if node is not None and node.left is None and node.right is None:
+            if level not in left_dict.keys():
+                left_dict[level] = node.val
+        # 然后下进一层，继续看是否有符合的叶子，同时增加层级
         dfs(node.left, level+1)
         dfs(node.right, level+1)
 
     dfs(root, 0)
-    print(left_dict)
+    # 找出最大层级对应的节点值
+    return left_dict[max(left_dict.keys())]
 
 
 if __name__ == "__main__":
     input = TreeNode(1, TreeNode(2), TreeNode(3, TreeNode(5)))
-    findBottomLeftValue(input)
-    #output = (input)
-    #print(output)
+    output = findBottomLeftValue(input)
+    print(output)
 
 
 
