282def49a1e8e477d9566f4ca1418e26b3a98be3
shenfeng <shenfeng0571@gmail.com>
2021-09-08 22:17:12 +0800

    0701,使用while不断往符合的分枝查找

---
+++ 0701_insertIntoBST/0701_insertIntoBST.py
@@ -0,0 +1,34 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def insertIntoBST(root: TreeNode, val: int):
+    if not root or not val:
+        return None
+    node = root
+    direction = 'left'
+    while node:
+        # 记录遍历到的位置
+        pre = node
+        if val < node.val:
+            node = node.left
+            direction = 'left'
+        else:
+            node = node.right
+            direction = 'right'
+
+    if direction == 'left':
+        pre.left = TreeNode(val)
+    else:
+        pre.right = TreeNode(val)
+
+    return root
+
+
+if __name__ == '__main__':
+    input = TreeNode(2, TreeNode(1), TreeNode(3))
+    output = insertIntoBST(input, 4)
+    print(output)
