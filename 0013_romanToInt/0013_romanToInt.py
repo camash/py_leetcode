0f48f5f6d885592965710694de3861f75e27d9ab
shenfeng <shenfeng0571@gmail.com>
2021-09-11 00:05:41 +0800

    0013,修改长度溢出的bug

--- 0013_romanToInt/0013_romanToInt.py
+++ 0013_romanToInt/0013_romanToInt.py
@@ -2,42 +2,42 @@ def romanToInt(s: str):
     res = 0
     while s:
         if s[-1] == 'X':
-            if s[-2] == 'I':
+            if len(s)>1 and s[-2] == 'I':
                 res += 9
                 s = s[:-2]
             else:
                 res += 10
                 s = s[:-1]
         elif s[-1] == 'V':
-            if s[-2] == 'I':
+            if len(s)>1 and s[-2] == 'I':
                 res += 4
                 s = s[:-2]
             else:
                 res += 5
                 s = s[:-1]
         elif s[-1] == 'C':
-            if s[-2] == 'X':
+            if len(s)>1 and s[-2] == 'X':
                 res += 90
                 s = s[:-2]
             else:
                 res += 100
                 s = s[:-1]
         elif s[-1] == 'L':
-            if s[-2] == 'X':
+            if len(s)>1 and s[-2] == 'X':
                 res += 40
                 s = s[:-2]
             else:
                 res += 50
                 s = s[:-1]
         elif s[-1] == 'M':
-            if s[-2] == 'C':
+            if len(s)>1 and s[-2] == 'C':
                 res += 900
                 s = s[:-2]
             else:
                 res += 1000
                 s = s[:-1]
         elif s[-1] == 'D':
-            if s[-2] == 'C':
+            if len(s)>1 and s[-2] == 'C':
                 res += 400
                 s = s[:-2]
             else:
@@ -51,7 +51,7 @@ def romanToInt(s: str):
 
 
 if __name__ == "__main__":
-    input = 'III'
+    input = 'LVIII'
     output = romanToInt(input)
     print(output)
 
