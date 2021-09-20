e7097cba7c27fe946a7f0fc1850b6aa4d6aeec42
shenfeng <shenfeng0571@gmail.com>
2021-09-21 00:53:20 +0800

    0036，bug fix

--- 0036_isValidSudoku/0036_isValidSudoku.py
+++ 0036_isValidSudoku/0036_isValidSudoku.py
@@ -8,8 +8,8 @@ def isValidSudoku(board: [[]]):
                 temp_dict[board[i][j]] += 1
             else:
                 temp_dict[board[i][j]] = 1
-        for k in range(9):
-            if temp_dict[board[i][j]] > 1:
+        for k in temp_dict.keys():
+            if k != '.' and temp_dict[k] > 1:
                 return False
 
     # 列比较
@@ -20,23 +20,29 @@ def isValidSudoku(board: [[]]):
                 temp_dict[board[j][i]] += 1
             else:
                 temp_dict[board[j][i]] = 1
-        for k in range(9):
-            if temp_dict[board[j][i]] > 1:
+        for k in temp_dict.keys():
+            if k != '.' and temp_dict[k] > 1:
                 return False
 
     # 方块比较
     seg_list = [[0,1,2], [3,4,5], [6,7,8]]
     for item_x in seg_list:
         for item_y in seg_list:
+            temp_dict.clear()
             for i in item_x:
-                temp_dict.clear()
                 for j in item_y:
                     if board[i][j] in temp_dict:
                         temp_dict[board[i][j]] += 1
                     else:
                         temp_dict[board[i][j]] = 1
-            for k in range(9):
-                if temp_dict[k] > 1:
+            for k in temp_dict.keys():
+                if k != '.' and temp_dict[k] > 1:
                     return False
 
     return True
+
+
+if __name__ == '__main__':
+    input = [[".",".",".",".","5",".",".","1","."],[".","4",".","3",".",".",".",".","."],[".",".",".",".",".","3",".",".","1"],["8",".",".",".",".",".",".","2","."],[".",".","2",".","7",".",".",".","."],[".","1","5",".",".",".",".",".","."],[".",".",".",".",".","2",".",".","."],[".","2",".","9",".",".",".",".","."],[".",".","4",".",".",".",".",".","."]]
+    output = isValidSudoku(input)
+    print(output)
