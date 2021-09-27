9ad6eff64be18a7de22948a18fe54b917213b4a6
shenfeng_air <shenfeng0571@gmail.com>
2021-09-27 20:40:03 +0800

    0043, add a str add function.

--- 0043_字符串相乘/0043_multiply.py
+++ 0043_字符串相乘/0043_multiply.py
@@ -1,33 +1,53 @@
 def multiply(num1: str, num2: str):
-    full_res = []
+    full_res = ""
     num1 = num1[::-1]
     num2 = num2[::-1]
+    # 从个位开始相乘
     for i in range(len(num1)):
-        curr_str_list = []
+        curr_str = ""
         curr = 0
         promo = 0
+        # 从个位开始相乘
         for j in range(len(num2)):
             promo, curr = divmod(int(num1[i]) * int(num2[j]) + promo, 10)
-            curr_str_list = [curr] + curr_str_list
-        full_res.append(curr_str_list + [0]*i)
-
-    promo_list = [0]*(len(num1)+len(num2))
-    for i in range(len(num1)+len(num2), -1, -1):
-        curr_digit = 0
-        for item in full_res:
-            if not item:
-                curr_digit += item.pop()
-
-
-
+            curr_str = str(curr) + curr_str
+        if promo > 0:
+            curr_str = str(promo) + curr_str
+        full_res = add_two_strings((curr_str + "0"*i), full_res)
 
 
     return full_res
 
 
+# 实现字符串相加的算法
+def add_two_strings(str1: str, str2: str):
+    n1 = len(str1)
+    n2 = len(str2)
+    if n1 < n2:
+        add_two_strings(str2, str1)
+    else:
+        str1 = str1[::-1]
+        str2 = str2[::-1]
+        promo = 0
+        curr = 0
+        res = ""
+        for i in range(n1):
+            if i < n2:
+                promo, curr = divmod(int(str1[i]) + int(str2[i]) + promo, 10)
+            else:
+                promo, curr = divmod(int(str1[i]) + promo, 10)
+            # 当前位拼装
+            res = str(curr) + res
+            if promo > 0:
+                res = str(promo) + res
+    return res
+
+
+
 if __name__ == "__main__":
-    input1 = "12"
-    input2 = "3"
+    input1 = "123"
+    input2 = "56"
+    #output = add_two_strings(input1, input2)
     output = multiply(input1, input2)
     print(output)
 
